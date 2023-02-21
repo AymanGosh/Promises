185958ddba2407053944f150ba387727f5086bf1
Ayman Abadlrhman <65545238+AymanGosh@users.noreply.github.com>
2023-02-21 23:43:58 +0200

    Update promises.js

--- promises.js
+++ promises.js
@@ -22,3 +22,35 @@ const finish=function(data){
 }
 
 getWeather().then(getIcon).then(finish)//"done11 done22 data33"
+
+///////////////////////////////////////////
+
+const getWeather2=function(){
+  let x  
+  setTimeout(function(){
+      x="done11"
+    },3000)
+  console.log(x)
+  return x;
+} 
+
+
+const getIcon2=function(data){
+  let y;  
+  setTimeout(function(){
+      y=data+" done22"
+    },2000)
+  console.log(y)
+  return y
+} 
+
+const finish2=function(data){
+    console.log(data+"  data33")
+}
+
+const x1=getWeather2();
+const x2=getIcon2(x1);
+finish2(x2)
+// undefined
+// undefined
+//"undefined  data33"
